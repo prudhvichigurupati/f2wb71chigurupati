9650663a4489ca51a0499a1ade7d2f1a33a46144
prudhvichigurupati <prudhvichigurupati88@gmail.com>
2022-11-02 18:14:36 -0500

    modifies watch

--- routes/watch.js
+++ routes/watch.js
@@ -3,7 +3,7 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('university', { title: 'Search Results'});
+  res.render('watch', { title: 'Search Results'});
 });
 
 module.exports = router;
